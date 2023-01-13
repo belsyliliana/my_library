671ad66698b9ff96c7050b99b248b13459f4bdd7
belsyliliana <belsyliliana2001@gmail.com>
2023-01-12 20:41:22 -0500

    12 enero del 2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  slideOpt = {
+    initialSlide: 0, //slide inicial (primero) [0,1,2,3]
+    slidesPerView: 1, //configuramos un slide por vista
+    centerSlides: true, //que las slides enten centradas
+    speed: 400 //velocidad movimiento de los slides
+
+  }
 
   constructor() {}
 
